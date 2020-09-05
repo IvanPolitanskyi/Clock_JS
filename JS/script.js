8ca5b9a952d8f2fa5c0ab01c01153417b71d7379
Vanya Politanskiy <vanyapolitanskiy@gmail.com>
2020-09-05 18:19:29 +0300

    Delete useles code

--- JS/script.js
+++ JS/script.js
@@ -13,12 +13,3 @@ setInterval(() => {
     mn.style.transform = `rotateZ(${mm}deg)`;
     sc.style.transform = `rotateZ(${ss}deg)`;
 })
-
-let day = new Date();
-let hh = day.getHours() * 30;
-let mm = day.getMinutes() * deg;
-let ss = day.getSeconds() * deg;
-
-hr.style.transform = `rotateZ(${(hh)+(mm/12)}deg)`;
-mn.style.transform = `rotateZ(${mm}deg)`;
-sc.style.transform = `rotateZ(${ss}deg)`;
